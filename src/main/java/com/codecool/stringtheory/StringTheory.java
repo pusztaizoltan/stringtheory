671bc01fde158f94548d220ce543a2b66848040f
Jonee27 <jonee271@gmail.com>
2020-11-19 15:24:40 +0100

    StringTheory

--- src/main/java/com/codecool/stringtheory/StringTheory.java
+++ src/main/java/com/codecool/stringtheory/StringTheory.java
@@ -1,50 +1,111 @@
 package com.codecool.stringtheory;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringTheory {
 
-    private static boolean isPalindrome(String str){
-        String cleanedString = str.toLowerCase().replaceAll("[^A-Za-z]","");
-        String reversedString = new StringBuilder(cleanedString).reverse().toString();
-        return cleanedString.equals(reversedString);
+    private static boolean isPalindrome(String str) {
+        String str1 = str.toLowerCase().replaceAll("[^A-Za-z]", ""); // Make all letters lowercase, replace every non-letter characters with nothing
+        char[] test = new char[str1.length()];
+        int j = 0;
+        for (int i = str1.length() - 1; i > -1; i--) {
+            test[j] = str1.charAt(i);
+            j++;
+        }
+        String compare = new String(test);
+        return str1.equals(compare);
     }
 
     private static boolean isIsogram(String str) {
-        String cleanedString = str.toLowerCase().replaceAll("[^A-Za-z]","");
-        String uniqueChars = ofUniqueCharaters(cleanedString);
-        return cleanedString.length() == uniqueChars.length();
+        String str1 = str.toLowerCase().replaceAll("[^A-Za-z]", ""); // Make all letters lowercase, replace every non-letter characters with nothing
+        char[] test = str1.toCharArray();
+        int counter = 0;
+        for (char i : test) {
+            for (char j : test) {
+                if (i == j) {
+                    counter++;
+                }
+            }
+        }
+        return counter <= test.length;
     }
 
     private static boolean isPangram(String str) {
-        String alphabet = "qaywsxedcrfvtgbzhnujmikolp";
-        String cleanedString = str.toLowerCase().replaceAll("[^A-Za-z]","");
-        String uniqueChars = ofUniqueCharaters(cleanedString);
-        return alphabet.length() == uniqueChars.length();
+        String str1 = str.toLowerCase().replaceAll("[^A-Za-z]", ""); // Make all letters lowercase, replace every non-letter characters with nothing
+        List<Character> letter = new ArrayList<Character>();
+        int counter = 0;
+
+        for (char i : "abcdefghijklmnopqrstuvwxyz".toCharArray()) {
+            letter.add(i);
+        }
+
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < letter.size(); j++) {
+                if (str1.charAt(i) == letter.get(j)) {
+                    letter.remove(j);
+                    counter++;
+                }
+            }
+        }
+
+        return letter.size() <= 0;
     }
 
     private static boolean isAnagram(String str1, String str2) {
-        String[] cleanedCharacters1 = str1.toLowerCase().replaceAll("[^A-Za-z]","").split("");
-        String[] cleanedCharacters2 = str2.toLowerCase().replaceAll("[^A-Za-z]","").split("");
-        Arrays.sort(cleanedCharacters1);
-        Arrays.sort(cleanedCharacters2);
-        return Arrays.equals(cleanedCharacters1, cleanedCharacters2);
+        String strOne = str1.toLowerCase().replaceAll("[^A-Za-z]", ""); // Make all letters lowercase, replace every non-letter characters with nothing
+        String strTwo = str2.toLowerCase().replaceAll("[^A-Za-z]", ""); // Make all letters lowercase, replace every non-letter characters with nothing
+
+        int counter = 0;
+        if(!strOne.length() == strTwo.length()){
+            for (int i = 0; i < strOne.length();i++){
+                for (int j = 0; i < strTwo.length();i++){
+                    char tmp = strTwo.charAt(j);
+                    if(strOne.indexOf(j))
+                }
+            }
+        }
     }
 
     private static boolean isBlanagram(String str1, String str2) {
-        String cleanedCharacters1 = str1.toLowerCase().replaceAll("[^A-Za-z]","");
-        String cleanedCharacters2 = str2.toLowerCase().replaceAll("[^A-Za-z]","");
-        String temp1 = cleanedCharacters1;
-        String temp2 = cleanedCharacters2;
-        for (String i: cleanedCharacters2.split("")) {
-            temp1 = temp1.replaceFirst(i, "");
+        String strOne = str1.toLowerCase().replaceAll("[^A-Za-z]", ""); // Make all letters lowercase, replace every non-letter characters with nothing
+        String strTwo = str2.toLowerCase().replaceAll("[^A-Za-z]", ""); // Make all letters lowercase, replace every non-letter characters with nothing
+        char[] strOneCharArray = strOne.toCharArray();
+        char[] strTwoCharArray = strTwo.toCharArray();
+        int counter = 0;
+        List<Character> strOneCheck = new ArrayList<Character>();
+        List<Character> strTwoCheck = new ArrayList<Character>();
+
+        for (int i = 0; i < strOneCharArray.length; i++) {
+            strOneCheck.add(strOne.charAt(i));
+            strTwoCheck.add(strTwo.charAt(i));
         }
-        for (String i: cleanedCharacters1.split("")) {
-            temp2 = temp2.replaceFirst(i, "");
+
+        if (strOne.length() != strTwo.length()) {
+            return false;
         }
-        return temp1.length() == 1 && temp2.length() == 1;
+
+        for (int i = 0; i < strOneCharArray.length; i++) {
+            for (int j = 0; j < strTwoCharArray.length; j++) {
+                if (strOneCharArray[i] == strTwoCharArray[j]) {
+                    strOneCheck.set(j, 'X');
+                    strTwoCheck.set(i, 'X');
+
+                }
+            }
+        }
+
+        for (int i = 0; i < strOneCheck.size(); i++) {
+            if (strOneCheck.get(i) != 'X') {
+                counter++;
+            }
+            if (strTwoCheck.get(i) != 'X') {
+                counter++;
+            }
+        }
+        return counter == 1 || counter == 2;
     }
+
 
     public static void main(String[] args) {
         System.out.println(isPalindrome("Mr. Owl ate my metal worm")); // True
@@ -53,15 +114,5 @@ public class StringTheory {
         System.out.println(isPangram("The quick brown fox jumps over the lazy dog")); // True
         System.out.println(isAnagram("Justin Timberlake", "I'm a jerk but listen")); // True
         System.out.println(isBlanagram("Justin Timberlake", "I'm a berk but listen")); // True
-    }
-    // support function to reduce string to a continuation of unique characters
-    private static String ofUniqueCharaters(String str) {
-        String stringOfUniqueChars = str.chars()
-                .distinct()
-                .collect(StringBuilder::new,
-                        StringBuilder::appendCodePoint,
-                        StringBuilder::append)
-                .toString();
-        return stringOfUniqueChars;
     }
 }
